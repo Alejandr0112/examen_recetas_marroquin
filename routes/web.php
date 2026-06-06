efeb9e322342869bab5f5ab0c158a565f4164fe7
Alejandr0112 <erickalejandro0112@gmail.com>
2026-06-06 11:37:23 -0600

    Redirigir inicio al catalogo de recetas

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', '/recipes');
 Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.index');
